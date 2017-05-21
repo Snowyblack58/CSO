eaf5bbd6f66fdbdc8672e048ef440ccee2048723
JocelynYH <SheepMaster@Big-Seaweed.local>
2017-05-21 01:49:27 -0400

    Finalized concert page except for media content
    Concert pages finished - need better edited slideshow photos and video
    on pages though.

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,105 +1,26 @@
-images = ['IMG_2550_compressed.JPG',
-		  'IMG_2627_compressed.JPG',
-		  'IMG_2990_compressed.JPG',
-		  'IMG_3012_compressed.JPG',
-		  'IMG_9399_compressed.png',
-		  'IMG_9405_compressed.png',
-		  'IMG_9408_compressed.png',
-		  'IMG_9425_compressed.png',
-		  'IMG_9430_compressed.png'];
-index = 0;
-transitioning = false;
-slideshowInterval = setInterval(function(){slideshowLeft()}, 5000)
-intervalSet = true;
+var slideIndex = 1;
+showDivs(slideIndex);
 
-$(document).ready(function(){
-	$('.slideshow-inactive-left').attr('src','raw/' + images[(index + images.length - 1) % images.length]);
-	$('.slideshow-active').attr('src','raw/' + images[index]);
-	$('.slideshow-inactive-right').attr('src','raw/' + images[(index + images.length + 1) % images.length]);
-});
-
-function slideLeft(){
-//	$('.slideshow-active').animate({
-//		left: "-=100%"
-//	}, 500);
-	$('.slideshow-active').css('left','-100%')
-//	$('.slideshow-inactive-right').animate({
-//		left: "-=100%"
-//	}, 500, function(){
-	$('.slideshow-inactive-right').css('left','0')
-//		transitioning = false;	
-//	});
-	$('.slideshow-inactive-left').css('left','100%');
-	
-	$('.slideshow-inactive-left').addClass('in-transition-right');
-	$('.slideshow-active').addClass('in-transition-left');
-	$('.slideshow-inactive-right').addClass('in-transition-active');
-	$('.in-transition-left').removeClass().addClass('slideshow-inactive-left');
-	$('.in-transition-active').removeClass().addClass('slideshow-active');
-	$('.in-transition-right').removeClass().addClass('slideshow-inactive-right');
-	$('.slideshow-inactive-right').attr('src', 'raw/' + images[(index + images.length + 1) % images.length])
-	transitioning = false;
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
 
-function slideshowLeft(){
-	if(!transitioning){
-		transitioning = true;
-		index = (index + images.length + 1) % images.length;
-		slideLeft();
-	}
+function currentDiv(n) {
+  showDivs(slideIndex = n);
 }
 
-$('.slideshow-left').click(function(){
-	slideshowRight();
-	clearInterval(slideshowInterval);
-	intervalSet = false;
-	setTimeout(function(){
-		if(!intervalSet){
-			intervalSet = true;
-			slideshowInterval = setInterval(function(){slideshowLeft()}, 5000)
-		}
-	}, 15000)
-});
-
-function slideRight(){
-//	$('.slideshow-active').animate({
-//		left: "+=100%"
-//	}, 500);
-	$('.slideshow-active').css('left','100%')
-//	$('.slideshow-inactive-left').animate({
-//		left: "+=100%"
-//	}, 500, function(){
-//		transitioning = false;	
-//	});
-	$('.slideshow-inactive-left').css('left','0')
-	$('.slideshow-inactive-right').css('left','-100%');
-	
-	$('.slideshow-inactive-left').addClass('in-transition-active');
-	$('.slideshow-active').addClass('in-transition-right');
-	$('.slideshow-inactive-right').addClass('in-transition-left');
-	$('.in-transition-left').removeClass().addClass('slideshow-inactive-left');
-	$('.in-transition-active').removeClass().addClass('slideshow-active');
-	$('.in-transition-right').removeClass().addClass('slideshow-inactive-right');
-	$('.slideshow-inactive-left').attr('src', 'raw/' + images[(index + images.length - 1) % images.length])
-	transitioning = false;
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+     dots[i].className = dots[i].className.replace(" w3-white", "");
+  }
+  x[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " w3-white";
 }
-
-function slideshowRight(){
-	if(!transitioning){
-		transitioning = true;
-		index = (index + images.length - 1) % images.length;
-		slideRight();
-	}
-}
-
-$('.slideshow-right').click(function(){
-	slideshowLeft();
-	clearInterval(slideshowInterval);
-	intervalSet = false;
-	setTimeout(function(){
-		if(!intervalSet){
-			intervalSet = true;
-			slideshowInterval = setInterval(function(){slideshowLeft()}, 5000)
-		}
-	}, 15000)	
-});
