f05eaf698a45c52f0d72472c52f0e21bfbc1b122
David Zhao <davidzhao058@gmail.com>
2017-08-12 20:03:17 -0400

    Use url hash to store  selected content name

--- js/app/dashboard.js
+++ js/app/dashboard.js
@@ -5,9 +5,22 @@ define([
     'text!../../partials/form-content.html',
     'json!../../json/dashboard-forms.json'
 ], function($, _, CardContainerTemplate, FormsTemplate, FormsJson){
+    var lookupTemplateAndJson = {
+        'default': {
+            'template': JSON.stringify(FormsTemplate),
+            'json': JSON.stringify(FormsJson)
+        },
+        'forms': {
+            'template': JSON.stringify(FormsTemplate),
+            'json': JSON.stringify(FormsJson)
+        }
+    }
+
     $(document).ready(function(){
         createCardView();
+        loadCardViewContent();
         loadCardViewContent(FormsTemplate, FormsJson);
+        syncHash();
     });
 
     function createCardView() {
@@ -16,9 +29,24 @@ define([
         $('#main-content').append(newCardContainerHtml);
     }
 
-    function loadCardViewContent(template, json) {
-        var newContentTemplate = _.template(template);
-        var newContentHtml = newContentTemplate(json);
+    function loadCardViewContent() {
+        var selectedContent = window.location.hash;
+        if(!$('#content-selector option[name="' + selectedContent.substring(1) + '"]').length > 0) {
+            selectedContent = '#default'
+        }
+        var obj = lookupTemplateAndJson[selectedContent.substring(1)];
+        if(obj === undefined){
+            window.location.hash = '';
+            obj = lookupTemplateAndJson[''];
+        }
+        var newContentTemplate = _.template(JSON.parse(obj.template));
+        var newContentHtml = newContentTemplate(JSON.parse(obj.json));
         $('.content-container').html(newContentHtml);
+        window.location.hash = $('#content-selector option:selected').attr('name');
+    }
+
+    function syncHash() {
+        var selectedOption = $('#content-selector option:selected').attr('name');
+        window.location.hash = '#' + selectedOption;
     }
 });
