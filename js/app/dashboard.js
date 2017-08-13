6af4e7173c65257ae3b200bf7957c7cfd2bccfea
David Zhao <davidzhao058@gmail.com>
2017-08-12 21:44:21 -0400

    Fix hash syncing

--- js/app/dashboard.js
+++ js/app/dashboard.js
@@ -2,8 +2,10 @@ define([
     'jquery',
     'underscore',
     'text!../../partials/form-content.html',
-    'json!../../json/dashboard-forms.json'
-], function($, _, FormsTemplate, FormsJson){
+    'json!../../json/dashboard-forms.json',
+    'text!../../partials/audition-content.html',
+    'json!../../json/dashboard-audition.json'
+], function($, _, FormsTemplate, FormsJson, AuditionTemplate, AuditionJson){
     var lookupTemplateAndJson = {
         'default': {
             'template': JSON.stringify(FormsTemplate),
@@ -12,19 +14,26 @@ define([
         'forms': {
             'template': JSON.stringify(FormsTemplate),
             'json': JSON.stringify(FormsJson)
+        },
+        'audition': {
+            'template': JSON.stringify(AuditionTemplate),
+            'json': JSON.stringify(AuditionJson)
         }
     }
 
     $(document).ready(function(){
+        syncHashOrSelect();
         loadCardViewContent();
-        syncHash();
+        syncHashOrSelect();
     });
 
     function loadCardViewContent() {
         var selectedContent = window.location.hash;
-        if(!$('#content-selector option[name="' + selectedContent.substring(1) + '"]').length > 0) {
+        console.log(selectedContent);
+        if($('#content-selector option[name=' + selectedContent.substring(1) + ']').length == 0) {
             selectedContent = '#default'
         }
+        console.log(selectedContent);
         var obj = lookupTemplateAndJson[selectedContent.substring(1)];
         if(obj === undefined){
             window.location.hash = '';
@@ -36,8 +45,14 @@ define([
         window.location.hash = $('#content-selector option:selected').attr('name');
     }
 
-    function syncHash() {
-        var selectedOption = $('#content-selector option:selected').attr('name');
-        window.location.hash = '#' + selectedOption;
+    function syncHashOrSelect() {
+        console.log(window.location.hash);
+        if(window.location.hash === ''){
+            var selectedOption = $('#content-selector option:selected').attr('name');
+            window.location.hash = selectedOption;
+        } else {
+            console.log($('#content-selector option[name=' + window.location.hash.substring(1) + ']'))
+            $('#content-selector option[name=' + window.location.hash.substring(1) + ']').attr('selected','selected');
+        }
     }
 });
