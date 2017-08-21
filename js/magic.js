16ffb11532ee1107f55332cbd4a9eb2145e1ef77
JocelynYH <seaweed4@live.com>
2017-08-21 18:27:49 -0400

    Major styling updates to unify website
    Unified fonts and colors, edited concerts page

---
+++ js/magic.js
@@ -0,0 +1,23 @@
+$( document ).ready(function() {
+	$(function() {
+	  	$('#disclaimer').click(function() {
+			$('#disclaimer-body').slideToggle('slow')
+		});
+		// $('#disclaimer').click(function() {
+		// 	$('#disclaimer-body').hide('slow')
+		// });
+	});
+});
+
+$( document ).ready(function() {
+	$(function() {
+	  	$('#stay-updated').click(function() {
+			$('#email-form').slideToggle('slow')
+		});
+		// $('#disclaimer').click(function() {
+		// 	$('#disclaimer-body').hide('slow')
+		// });
+	});
+});
+
+		
