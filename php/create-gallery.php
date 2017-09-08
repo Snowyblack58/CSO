ae32f6d18a4b1dada6ed15f7adaf7c6c8ccf2bd4
David Zhao <davidzhao058@gmail.com>
2017-09-08 13:39:24 -0400

    Move all forms and logos into folders

--- php/create-gallery.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $dir = '../raw/headshots';
-    $filenames = scandir($dir);
-    foreach($filenames as $filename) {
-        if($filename != '.' && $filename != '..'){
-            echo '<div class="headshot col-sm-1"><img src=raw/headshots/' . $filename . '></div>';
-        }
-    }
-?>
--- php/members-auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-if(strtolower($_POST["email"]) == "dz6hu@virginia.edu"){
-	$data = ["isMember" => true];
-} else {
-	$data = ["isMember" => false];
-}
-echo json_encode($data);
-?>
