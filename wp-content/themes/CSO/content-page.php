f0ec8c26f0c2db6cd09a5475aba4f561829cadc5
David Zhao <davidzhao058@gmail.com>
2017-09-20 01:44:16 -0400

    Remove extra tagging in page template

--- wp-content/themes/CSO/content-page.php
+++ wp-content/themes/CSO/content-page.php
@@ -3,16 +3,4 @@
  * The template used for displaying page content in page.php
  */
 ?>
-
-	<article <?php post_class(); ?>>
-	
-		<header>
-			<?php the_post_thumbnail(); ?>
-			<h1><?php the_title(); ?></h1>
-		</header>
-
-		<div>
-			<?php the_content(); ?>
-		</div>
-		
-	</article>
+<?php the_content(); ?>
