1e0fdeee2afcac101d7328bed535d382804f4d49
David Zhao <davidzhao058@gmail.com>
2018-01-20 12:53:46 -0500

    Add tunnel to members revamped for wordpress customization

--- wp-content/themes/CSO/footer.php
+++ wp-content/themes/CSO/footer.php
@@ -1,24 +1,25 @@
-<?php
-/**
- * The template for displaying the footer.
- */
-?>
-
-        <footer>
-        	<div class="footer-logos">
-            	<div class="footer-logo" id="youtube">
-					<a href="https://www.youtube.com/channel/UCyjBce2H3vyBmLYxhEZpyJA" target="_blank">
-						<img src="http://www.cavalierso.com/wp-content/uploads/2017/10/YouTube-logo-dark.png">
-					</a>
-				</div>
-			</div>
-			<div class="footer-links">
-				<a href="/about">About</a>|
-				<a href="/support">Donate</a>|
-				<a href="mailto:dz6hu@virginia.edu">Contact Us</a>|
-				<a href="/disclaimer">Disclaimer</a>|
-				<a href="mailto:dz6hu@virginia.edu">Report a Bug</a>
-			</div>
-        </footer >
-    </body>
+<?php
+/**
+ * The template for displaying the footer.
+ */
+?>
+
+        <footer>
+        	<div class="footer-logos">
+            	<div class="footer-logo" id="youtube">
+					<a href="https://www.youtube.com/channel/UCyjBce2H3vyBmLYxhEZpyJA" target="_blank">
+						<img src="http://www.cavalierso.com/wp-content/uploads/2017/10/YouTube-logo-dark.png">
+					</a>
+				</div>
+			</div>
+			<div class="footer-links">
+				<a href="/about">About</a>|
+				<a href="/support">Donate</a>|
+				<a href="mailto:dz6hu@virginia.edu">Contact Us</a>|
+				<a href="/disclaimer">Disclaimer</a>|
+				<a href="mailto:dz6hu@virginia.edu">Report a Bug</a>|
+				<a href="/members-revamped">Dev</a>
+			</div>
+        </footer >
+    </body>
 </html>
